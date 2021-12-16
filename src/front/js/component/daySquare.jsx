5a1ab937f18c60c44a7de0e01f4bad591bdfe116
willy5360 <willy5360@hotmail.com>
2021-12-16 16:57:54 +0000

    finished widgetmember

--- src/front/js/component/daySquare.jsx
+++ src/front/js/component/daySquare.jsx
@@ -2,34 +2,37 @@ import React from "react";
 import PropTypes from "prop-types";
 import HolidayCircle from "./holidayCircle.jsx";
 
-const DaySquare = props => {
-	const clicked = () => {
-		window.alert("You've clicked me");
-	};
-	if (props.holidayName){
-		return(
-			<div className={props.istoday.concat(props.isNumberOne)} onClick={""}>
-				<HolidayCircle />
-				<span className="day_square--holidayName"><div>{props.holidayName}</div></span>			
-				<span className="day_square--dayNumber">{props.day}</span>
-			</div>
-		)
-	}else {
-		return (
-			<div className={props.istoday.concat(props.isNumberOne)} onClick={""}>
-				<span className="day_square--holidayName"><div>{props.holidayName}</div></span>			
-				<span className="day_square--dayNumber">{props.day}</span>
-				
-			</div>
-		);
-	}
+const DaySquare = (props) => {
+    const clicked = () => {
+        window.alert("You've clicked me");
+    };
+    if (props.holidayName) {
+        return (
+            <div className={props.istoday.concat(props.isNumberOne)}>
+                <HolidayCircle />
+                <span className="day_square--holidayName">
+                    <div>{props.holidayName}</div>
+                </span>
+                <span className="day_square--dayNumber">{props.day}</span>
+            </div>
+        );
+    } else {
+        return (
+            <div className={props.istoday.concat(props.isNumberOne)}>
+                <span className="day_square--holidayName">
+                    <div>{props.holidayName}</div>
+                </span>
+                <span className="day_square--dayNumber">{props.day}</span>
+            </div>
+        );
+    }
 };
 
 DaySquare.propTypes = {
-	day: PropTypes.number,
-	istoday: PropTypes.string,
-	isNumberOne: PropTypes.string,
-	holidayName:PropTypes.string
+    day: PropTypes.number,
+    istoday: PropTypes.string,
+    isNumberOne: PropTypes.string,
+    holidayName: PropTypes.string,
 };
 
 export default DaySquare;
