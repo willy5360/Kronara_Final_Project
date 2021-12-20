6d56354885e2bdbc2c9adf8026d90afba2937f7b
willy5360 <willy5360@hotmail.com>
2021-12-20 14:38:36 +0000

    Revert "pulling main"
    This reverts commit a88c5c9433a344c5239d3f9b4261d86aeadf0e24.

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,15 +1,13 @@
 import React from "react";
 import "../../styles/home.scss";
 import { Navbar } from "../component/navbar.js";
-import MonthSquare from "../component/monthSquare.jsx";
 
 export const Home = () => {
-    return (
-        <div className="container-home">
-            <div className="container-home-opacity">
-                <Navbar />
-                <MonthSquare />
-            </div>
-        </div>
-    );
+  return (
+    <div className="container-home">
+      <div className="container-home-opacity">
+        <Navbar />
+      </div>
+    </div>
+  );
 };
